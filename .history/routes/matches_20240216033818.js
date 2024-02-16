9ab11822e8a112d6fa23957217313b058a24c842
AmineBNLK <amine.bnlk@gmail.com>
2024-02-16 04:50:38 +0100

    changer les methodes de fetch pour ajouter populate

---
+++ .history/routes/matches_20240216033818.js
@@ -0,0 +1,34 @@
+const Joueur = require('../models/Joueur');
+
+const express = require('express');
+const {
+  getMatches,
+  getMatch,
+  createMatch,
+  updateMatch,
+  deleteMatch,
+  joinMatch,
+  leaveMatch,
+} = require('../controllers/matches');
+
+
+
+const router = express.Router();
+const Match = require('../models/Match');
+const advancedResults = require('../middleware/advancedResults');
+
+const { authorize } = require('../middleware/auth');
+
+router.route('/').get(advancedResults(Match), getMatches).post(createMatch);
+
+router
+  .route('/:id')
+  .get(getMatch)
+  .put(authorize('Joueur'), updateMatch)
+  .delete(authorize('Joueur', 'Admin'), deleteMatch);
+
+router.route('/:id/join').post(joinMatch);
+
+router.route('/:id/leave').post(leaveMatch);
+
+module.exports = router;
